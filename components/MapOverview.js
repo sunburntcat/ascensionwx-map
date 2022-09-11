69a046f4db4e28c9e1212e0a9602dac49e4a203d
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-09-11 19:51:54 +0100

    succeed to add circle and text to the Map

--- components/MapOverview.js
+++ components/MapOverview.js
@@ -16,6 +16,13 @@ const MapOverview = () => {
     })
   }, [])
 
+  const tIcon = ({props}) => {
+    return <Marker center={[0, 0]} icon={L.divIcon({
+      className: "text-red-600",
+      html: "15",
+      iconAnchor: [4, 5]
+    })}/>
+  }
   
   return (
     <MapContainer center={[6.524400, 3.379199]} zoom={3} scrollWheelZoom={true} style={{height: "100%", width: "100%"}}>
@@ -25,12 +32,23 @@ const MapOverview = () => {
       />
       {
         rows.map((row) => (
-          <CircleMarker key={row.longitude_deg+row.latitude_deg} center={[row.latitude_deg , row.longitude_deg]} radius={20} >
-            <Popup >
-              Kanda.<br /> {row.latitude_deg} {row.longitude_deg}.
-               <div className='h-52 w-52'></div>
-            </Popup>
-          </CircleMarker>
+          <>
+            <CircleMarker key={row.longitude_deg+row.latitude_deg} center={[row.latitude_deg , row.longitude_deg]} 
+                        radius={15} color="black" fillColor='yellow' opacity={1} 
+                        fillOpacity={1} weight={2}
+                >
+              <Popup >
+                Kanda.<br /> {row.latitude_deg} {row.longitude_deg}.
+                <div className='h-52 w-52 '></div>
+              </Popup>
+            </CircleMarker>
+            <Marker position={[row.latitude_deg, row.longitude_deg]} icon={L.divIcon({
+                className: "text-black font-bold",
+                html: "15",
+                // iconAnchor: [4, 5]
+              })}/>
+          </>
+          
         ))
       }
     </MapContainer>
