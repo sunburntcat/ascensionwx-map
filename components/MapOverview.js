5b9246862da39f7b606de02652dcc71ae3c5b6ec
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-09-12 04:11:40 +0100

    adding the blue loader

--- components/MapOverview.js
+++ components/MapOverview.js
@@ -1,5 +1,7 @@
 import { MapContainer, Marker, Popup, TileLayer, CircleMarker } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
+import Image from 'next/image'
+import BlueLoader from "../public/loader.gif"
 import Temperature from "../components/Chart/Temperature"
 import { useEffect, useState } from 'react'
 import { postMapData, getActions, checkCurrentGMT, compare, getData, diff_days } from '../lib/api'
@@ -9,13 +11,15 @@ import { postMapData, getActions, checkCurrentGMT, compare, getData, diff_days }
 const MapOverview = () => {
   
   ////////// STATES ///////////////
-  const [series, setSeries] = useState([])
   const [rows, setRows] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
 
   // fetch MapData from Telos chain
   useEffect( () => {
     const fetcher = async () => {
       // fetch post Map data
+      setIsLoading(true)
       const data = await postMapData()
       setRows(data.rows)
 
@@ -34,7 +38,7 @@ const MapOverview = () => {
       }
       setRows(devs)
       console.log(devs)
-      
+      setIsLoading(false)
     }
     
     fetcher()
@@ -53,65 +57,76 @@ const MapOverview = () => {
   }
   
   return (
-    <MapContainer center={[6.524400, 3.379199]} zoom={3} scrollWheelZoom={true} style={{height: "100%", width: "100%"}}>
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {
-        rows.map((row) => (
-          <>
-            <Marker position={[row.latitude_deg, row.longitude_deg]} icon={L.divIcon({
-                className: "text-black font-medium",
-                html: "15",
-              })}>
-                <Popup maxWidth={700} maxHeight={600}>
-                  <div className='h-[500px] w-[700px]'>
-                    <p className="flex space-x-3"> 
-                        <span className="flex gap-2 text-4xl font-normal leading-tight text-gray-500 ">
-                            Devname:
-                            <span className="text-4xl font-semibold leading-tight text-blue-500 ">
-                                {row.devname}
-                            </span>
-                        </span>
-                    </p>
-                    <div className='flex gap-5 ml-5'>
-                        <p className="flex space-x-3">
-                            <span className="flex gap-2 text-base font-normal leading-tight text-gray-500 ">
-                                Lat: 
-                                <span className="text-base font-semibold leading-tight text-gray-500 ">
-                                    {row.latitude_deg}
-                                </span>
-                            </span>
-                        </p>
-                        <p className="flex space-x-3">
-                            <span className="flex gap-2 text-base font-normal leading-tight text-gray-500 ">
-                                Long: 
-                                <span className="text-base font-semibold leading-tight text-gray-500 ">
-                                    {row.longitude_deg}
-                                </span>
-                            </span>
-                        </p>
+  <div>
+    {
+      (isLoading) 
+      ?
+      <div>
+        <Image src={BlueLoader} alt="Loading GIF" className='justify-center items-center'/>
+      </div>
+      :
+      <MapContainer center={[6.524400, 3.379199]} zoom={3} scrollWheelZoom={true} style={{height: "100%", width: "100%"}}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {
+          rows.map((row) => (
+            <>
+              <Marker position={[row.latitude_deg, row.longitude_deg]} icon={L.divIcon({
+                  className: "text-black font-medium",
+                  html: "15",
+                })}>
+                  <Popup maxWidth={700} maxHeight={600}>
+                    <div className='h-[500px] w-[700px]'>
+                      <p className="flex space-x-3"> 
+                          <span className="flex gap-2 text-4xl font-normal leading-tight text-gray-500 ">
+                              Devname:
+                              <span className="text-4xl font-semibold leading-tight text-blue-500 ">
+                                  {row.devname}
+                              </span>
+                          </span>
+                      </p>
+                      <div className='flex gap-5 ml-5'>
+                          <p className="flex space-x-3">
+                              <span className="flex gap-2 text-base font-normal leading-tight text-gray-500 ">
+                                  Lat: 
+                                  <span className="text-base font-semibold leading-tight text-gray-500 ">
+                                      {row.latitude_deg}
+                                  </span>
+                              </span>
+                          </p>
+                          <p className="flex space-x-3">
+                              <span className="flex gap-2 text-base font-normal leading-tight text-gray-500 ">
+                                  Long: 
+                                  <span className="text-base font-semibold leading-tight text-gray-500 ">
+                                      {row.longitude_deg}
+                                  </span>
+                              </span>
+                          </p>
+                      </div>
+                      <div className='h-1 w-full bg-gray-200'/>
+                      <Temperature values={rows}/>
                     </div>
-                    <div className='h-1 w-full bg-gray-200'/>
-                  </div>
-                </Popup>
-            </Marker>
-            <CircleMarker key={row.longitude_deg+row.latitude_deg} center={[row.latitude_deg , row.longitude_deg]} 
-                        radius={15} color="black" fillColor={rcolor()} opacity={1} 
-                        fillOpacity={1} weight={1.2}
-                >
-              {/* <Popup >
-                Kanda.<br /> {row.latitude_deg} {row.longitude_deg}.
-                <div className='h-52 w-52 '></div>
-              </Popup> */}
-            </CircleMarker>
+                  </Popup>
+              </Marker>
+              <CircleMarker key={row.longitude_deg+row.latitude_deg} center={[row.latitude_deg , row.longitude_deg]} 
+                          radius={15} color="black" fillColor={rcolor()} opacity={1} 
+                          fillOpacity={1} weight={1.2}
+                  >
+                {/* <Popup >
+                  Kanda.<br /> {row.latitude_deg} {row.longitude_deg}.
+                  <div className='h-52 w-52 '></div>
+                </Popup> */}
+              </CircleMarker>
+              
+            </>
             
-          </>
-          
-        ))
-      }
-    </MapContainer>
+          ))
+        }
+      </MapContainer>
+    }
+  </div>
   )
 }
 
