c4d59b09256aaa8678ac4ab3fe09cee7e878be1b
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-09-12 01:59:41 +0100

    added diff_

--- components/MapOverview.js
+++ components/MapOverview.js
@@ -2,8 +2,31 @@ import { MapContainer, Marker, Popup, TileLayer, CircleMarker, Circle } from 're
 import 'leaflet/dist/leaflet.css'
 import { useEffect, useState } from 'react'
 // import L from "leaflet"
-import { postMapData } from '../lib/api'
+import { postMapData, getActions, checkCurrentGMT, compare, getData, diff_days } from '../lib/api'
 
+function diff_(d) {
+  const today = new Date();
+  d = new Date(d);
+  const days = parseInt((today - d) / (1000 * 60 * 60 * 24));
+  const hours = parseInt(Math.abs(today - d) / (1000 * 60 * 60) % 24);
+  const minutes = parseInt(Math.abs(today.getTime() - d.getTime()) / (1000 * 60) % 60);
+  const seconds = parseInt(Math.abs(today.getTime() - d.getTime()) / (1000) % 60); 
+
+  console.log("─────────────────────")
+  if(days == 0) {
+    if(hours == 0) {
+      console.log("minutes: "+ minutes)
+      return minutes + " minute(s) ago"
+    }else{
+      console.log("hours: "+ hours)
+      return hours + " hours(s) ago"
+    }
+    
+  } else {
+    console.log("days: "+ days)
+    return days + " day(s) ago"
+  }
+}
 
 
 const MapOverview = () => {
@@ -91,4 +114,86 @@ const MapOverview = () => {
   )
 }
 
-export default MapOverview
+
+// the puller data
+async function puller(context) {
+
+  // get the start time and the name of the device sensor
+  let d = new Date();
+  
+  
+  ///////////////////////////// pre config ////////////////////////////
+  const _devname = context.sensor
+  var _before = context.before
+  if(!_before) _before = 5
+  // _before = _before+1
+  
+  d.setDate(d.getDate() - _before)
+  
+  // set the day to [today - 'before' days] in iso format
+  let start = d.toISOString()
+
+  // check GMT index time to different zone
+  var val = new Date().toString().match(/([-\+][0-9]+)\s/)[1] 
+  var id = checkCurrentGMT(val)
+  // if (id == 0 ){
+  //   // add 1hour to the server time
+    // d.setHours( d.getHours() + 1 )
+    start = d.toISOString()
+  // }
+  // d.setHours( d.getHours() + 1 )
+  // start = d.toISOString()
+  /////////////////////////////
+
+
+ 
+  // get the response data
+  const res = await getActions( start )
+
+  // parse into json format
+  const json = await res.json()
+  const _actions = json.actions
+
+  const parsed = getData(_actions, _devname)
+
+  const _data = JSON.stringify(parsed)
+
+  let is_data_collected = false
+
+  ////////////////////// check result of _data ////////////////////////////////////////
+  let existing_sensor = false
+  if(_data == JSON.stringify({temperature:[],humidity:[],pressure:[],times:[]})){
+    existing_sensor = false
+  }else{
+    existing_sensor = true
+
+    // is data collected statement ?
+    const size = parsed.times.length
+
+    if(size == 0){
+      is_data_collected = false
+    }else{
+      const from = parsed.times[size-1]
+      const day_threshold = start
+      is_data_collected = compare(from, day_threshold)
+
+    }
+  }
+  //////////////////////////////////////////////
+
+
+  return {
+    props: {
+      data: parsed || JSON.stringify({}), //_data
+      existing_sensor,
+      is_data_collected,
+
+    }
+  }
+}
+
+
+
+////////////////////////////////
+
+export default MapOverview;
