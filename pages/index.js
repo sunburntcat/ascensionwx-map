bb0eee96d9ffaf2ee5cde5883b961d305eab8c72
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-09-13 02:47:15 +0100

    removed unecessary file

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,22 @@
 
+import dynamic from 'next/dynamic';
+const MapOverview = dynamic(() => import("../components/MapOverview"), {
+    loading: () => "Loading...",
+    ssr: false
+});
+
+
+
 export default function Home() {
-  return (
-    <></>
-  )
-}
+    
+    return (
+        <div className="justify-center">
+
+            <main className="w-screen h-screen  bg-white">
+                <MapOverview className={""}/>
+            </main>
+            <br/>
+        </div>
+        
+    )
+} 
